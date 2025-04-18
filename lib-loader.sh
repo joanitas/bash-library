b530d8c3157f523bbd810d92e6433fa1cee2bc2b
hperezrodal <hperezrodal@gmail.com>
2025-04-17 21:12:28 -0300

    fix: remove unimplemented modules

--- lib-loader.sh
+++ lib-loader.sh
@@ -3,16 +3,9 @@
 # Entry point for the Bash library
 
 # Source all the modules from the `modules/` directory
-LIB_DIR="$(dirname "${BASH_SOURCE[0]}")/modules"
+LIB_DIR="$BASH_LIBRARY_PATH/modules"
 
 # Source each module
-source "$LIB_DIR/datetime.sh"
-source "$LIB_DIR/logging.sh"
-source "$LIB_DIR/git.sh"
-source "$LIB_DIR/validate_params.sh"
-source "$LIB_DIR/docker.sh"
-source "$LIB_DIR/pipeline.sh"
-source "$LIB_DIR/files.sh"
 source "$LIB_DIR/aws.sh"
 
 # Any additional setup can go here
